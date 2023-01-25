445b83963081dd73d5bfbde589341cf4e8ba1019
Elvira Sinyanskaya <esinyanskaya@gmail.com>
2023-01-25 20:11:20 +0200

    Adding function to display books

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let myLibrary = [];
 
-
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -11,18 +10,41 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
-  console.log(Book.title);
 }
 
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
-addBookToLibrary('Alice in Wonderland', 'L. Stevenson', 75, true);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("Alice in Wonderland", "L. Stevenson", 75, true);
+addBookToLibrary("The Bell Jar", "Sylvia Plath", 294, false);
 
-console.log(myLibrary);
+function showBooks() {
+  const container = document.querySelector(".container");
 
-const div = document.querySelector('.book');
-for (let i = 0; i < myLibrary.length; i++) {
-  div.textContent += myLibrary[i].title;
-  console.log(myLibrary[i]);
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = document.createElement("div");
+    book.classList.add("book");
+    const title = document.createElement("h1");
+    title.classList.add("book-title");
+    title.textContent = myLibrary[i].title;
+    book.appendChild(title);
+
+    const author = document.createElement("h3");
+    author.classList.add("book-author");
+    author.textContent = myLibrary[i].author;
+    book.appendChild(author);
+
+    const pages = document.createElement("p");
+    pages.classList.add("book-pages");
+    pages.textContent = myLibrary[i].pages + " pages";
+    book.appendChild(pages);
+
+    const read = document.createElement("p");
+    pages.classList.add("book-read");
+    myLibrary[i].read
+      ? (read.textContent = "read")
+      : (read.textContent = "not read yet");
+    book.appendChild(read);
+    container.appendChild(book);
+  }
 }
 
-
+showBooks();
