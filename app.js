5e2f4cec1f5a8c01c01b59b70ab77237d2ce2823
Elvira Sinyanskaya <esinyanskaya@gmail.com>
2023-01-28 20:33:45 +0200

    Adding function to toggle read status

--- app.js
+++ app.js
@@ -17,9 +17,6 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 // tests
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
-addBookToLibrary("Alice in Wonderland", "L. Stevenson", 75, true);
-addBookToLibrary("The Bell Jar", "Sylvia Plath", 294, false);
 
 //function that displays books on the page
 function showBooks() {
@@ -36,25 +33,53 @@ function showBooks() {
     book.classList.add("book");
 
     //creating remove book button
-    const removeBookBtn = document.createElement('button');
-    removeBookBtn.classList.add('remove-book-btn');
-    removeBookBtn.textContent = 'Remove';
-    
+    const removeBookBtn = document.createElement("button");
+    removeBookBtn.classList.add("remove-book-btn");
+    removeBookBtn.textContent = "Remove";
+
     //link the data attribute of the remove button to the array and a card
     removeBookBtn.dataset.index = index;
-    index++;
+
     book.appendChild(removeBookBtn);
 
     //adding event listener and removing item from the parent div
-
-    removeBookBtn.addEventListener('click', removeBookFromLibrary);
+    removeBookBtn.addEventListener("click", removeBookFromLibrary);
 
     function removeBookFromLibrary() {
-      let bookIndex = parseInt(removeBookBtn.dataset.index);
-      myLibrary.splice(bookIndex, 1);
+      let bookIndexToRemove = parseInt(removeBookBtn.dataset.index);
+      console.log(libraryItem);
+      myLibrary.splice(bookIndexToRemove, 1);
       showBooks();
     }
-    // loop throught keys to display each on the book card
+
+    //Create read status button and add class attribute for each array card
+    const readStatusBtn = document.createElement("button");
+    readStatusBtn.classList.add("read-status-btn");
+    // link value of read to the button
+    for (let key in libraryItem) {
+      if (key === "read") {
+        if (libraryItem[key] === false) {
+          readStatusBtn.textContent = "Mark as read";
+        } else readStatusBtn.textContent = "Mark as not read";
+      }
+    }
+    book.appendChild(readStatusBtn);
+
+    //add event listener and toggle read status
+    readStatusBtn.addEventListener("click", toggleReadStatus);
+
+    //change value of read when the button is clicked
+    function toggleReadStatus() {
+        for (let key in libraryItem) {
+          if (key === "read") {
+            libraryItem[key] = !libraryItem[key];
+          }
+        }
+      // update for correct display of read status
+      showBooks();
+    }
+
+    // loop through keys to display each on the book card
     for (let key in libraryItem) {
       // console.log(`${key}: ${myLibrary[key]}`);
       const text = document.createElement("p");
@@ -70,10 +95,11 @@ function showBooks() {
       }
       book.appendChild(text);
     }
+    index++;
+    console.log(readStatusBtn.dataset.index);
     container.appendChild(book);
   });
 }
-showBooks();
 
 // display form after clicking the 'Add new book' button
 const buttonAddBook = document.querySelector(".new-book-btn");
@@ -82,7 +108,6 @@ buttonAddBook.addEventListener("click", displayForm);
 function displayForm() {
   document.querySelector(".new-book").classList.add("opened");
 }
-
 
 // intake the data user have entered
 document
@@ -104,3 +129,8 @@ document
     //reset the form after successful submission
     document.querySelector("#new-book-form").reset();
   });
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("Alice in Wonderland", "L. Stevenson", 75, true);
+addBookToLibrary("The Bell Jar", "Sylvia Plath", 294, false);
+
